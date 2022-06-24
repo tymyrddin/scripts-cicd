9d30329f00439101d4aae1466524db9dce305a25
ninabarzh <30133030+ninabarzh@users.noreply.github.com>
2022-06-24 03:12:42 +0200

    Jenkins credentials

---
+++ groovy/README.md
@@ -0,0 +1,9 @@
+# Jenkins groovy scripts for credentials
+
+Groovy is an object-oriented, static and dynamic language whose features are similar to Python. 
+It was first released in January 2007.
+
+Test these scripts using the Jenkins script console:
+
+* [Jenkins all credentials](jenkins-all-creds.groovy)
+* [Jenkins dump credentials](jenkins-dump-credentials.groovy)
---
+++ groovy/jenkins-all-creds.groovy
@@ -0,0 +1,15 @@
+import jenkins.*
+import jenkins.model.*
+import hudson.*
+import hudson.model.*
+
+def jenkinsCredentials = com.cloudbees.plugins.credentials.CredentialsProvider.lookupCredentials(
+        com.cloudbees.plugins.credentials.Credentials.class,
+        Jenkins.instance,
+        null,
+        null
+);
+
+for (creds in jenkinsCredentials) {
+    println(jenkinsCredentials.id)
+    }
